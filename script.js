48bf4f9324f8a5624b6433b900d3e22d8f5cd922
Daniel Corcoran <danielc.92@outlook.com>
2019-05-27 09:58:20 +1000

    Added script and index

---
+++ script.js
@@ -0,0 +1,11 @@
+/* GRAMMAR AND TYPES
+SOURCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Grammar_and_Types */
+
+// Declarations
+var name = 'Daniel';
+const setting = true;
+let age = 50;
+
+// Evaluating variables
+
+console.log()
