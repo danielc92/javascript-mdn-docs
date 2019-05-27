18a0c9f149f90f3012cb9aa6f6170ca5ad99ef9d
Daniel Corcoran <danielc.92@outlook.com>
2019-05-27 10:52:42 +1000

    Added functions.js

---
+++ functions.js
@@ -0,0 +1,13 @@
+/*
+FUNCTIONS
+SOURCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Functions
+*/
+
+
+
+// Defining functions
+function getName(name) {
+    console.log('The name is ', name);
+}
+
+getName('Daniel Corcoran');
