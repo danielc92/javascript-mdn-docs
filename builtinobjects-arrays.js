c5c5a21a848e7e49e5bd4f237f2963847464480d
Daniel Corcoran <danielc.92@outlook.com>
2019-05-27 11:20:54 +1000

    Creted built in objects: Arrays

---
+++ builtinobjects-arrays.js
@@ -0,0 +1,20 @@
+/* 
+BUILT IN OBJECTS: ARRAYS
+SOURCE: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
+*/
+
+
+
+// Creation
+let names = ['Daniel', 'George', 'Rajesh', 'Boerg'];
+
+
+
+// Index Access
+console.log(names[3]);
+
+
+
+// Getting Index
+console.log(name.indexOf('George'));
+
